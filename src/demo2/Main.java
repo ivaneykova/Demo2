cfaeac2749de437c527dd0c42897c1af7096c060
Iva Neykova <thisisiva@hotmail.com>
2012-01-26 19:10:50 -0500

    tache #1: print "Premiere modif"

---
+++ src/demo2/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo2;
+
+/**
+ *
+ * @author Ivelina
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+       System.out.println("Premiere modification");
+       System.out.println("Premiere modification");
+    }
+}
